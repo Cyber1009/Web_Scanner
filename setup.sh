66d14f4e9540270dc2efb20804d209639c6c9b31
Cyber1009 <36602907+Cyber1009@users.noreply.github.com>
2024-12-19 09:06:10 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Install necessary Python packages
+# Install dependencies
 pip install -r requirements.txt
 
 # Download NLTK resources
 python -m nltk.downloader wordnet
+python -m nltk.downloader omw-1.4
